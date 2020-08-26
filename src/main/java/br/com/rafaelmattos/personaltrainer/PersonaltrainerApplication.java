72170782656a238adc50e5646581b7900661e1be
Rafael Mattos <rafaelrj@live.com>
2020-08-26 02:21:15 -0300

    Projeto criado

---
+++ src/main/java/br/com/rafaelmattos/personaltrainer/PersonaltrainerApplication.java
@@ -0,0 +1,13 @@
+package br.com.rafaelmattos.personaltrainer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonaltrainerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersonaltrainerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/rafaelmattos/personaltrainer/PersonaltrainerApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.rafaelmattos.personaltrainer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonaltrainerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
